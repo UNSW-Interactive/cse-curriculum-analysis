389a50a036a5540248631b12b0711c44234b0c72
Terry Agapitos <terry@agapitos.com.au>
2020-07-14 00:40:17 +1000

    scraped prereqs from handbook

---
+++ server/course_scraper/prereq_generator.py
@@ -0,0 +1,85 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+with open('course_scraper/courses_nodupes.json') as myf:
+    j = json.load(myf)
+    aa = {}
+    for cc in j:
+        aa[cc['code']] = {
+            'offering_time': cc['offering_time'],
+            'name': cc['name'],
+            'url': cc['url'],
+            'outline': cc['outline'] if 'outline' in cc else ''
+        }
+
+def scrape_handbook():
+
+    handbook_url = 'https://www.handbook.unsw.edu.au/{}/courses/2020/{}/' # undergraduate, code
+    for course in j:
+        course_name = course['code']
+        for grad_level in ('undergraduate', 'postgraduate'):
+            url = handbook_url.format(grad_level, course_name.lower())
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            error_page = soup.find('div', {'class':'error-page'})
+            if error_page is not None:
+                continue
+            
+            print("-----")
+            print(course_name, grad_level)
+            subject_conditions = soup.find('div', {'id':'readMoreSubjectConditions'})
+            if subject_conditions is not None:
+                print(subject_conditions.div.div.text)
+
+
+# Then prereqs were parsed manually into Polish Notation
+def parse_tokens(tokens):
+    operators = {'A': "AND", 'O': "OR", 'C': "WITH"} # and/or/co-req
+    course_value = '' # Would be maybe the grade required, idk
+    stack = []
+    for token in tokens:
+        if token in operators:
+            if token == 'C':
+                coreq = stack.pop()
+                stack.insert(0, [operators[token], coreq])
+            else:
+                left = stack.pop()
+                right = stack.pop()
+                stack.insert(0, [operators[token], left, right])
+        else:
+            stack.insert(0, {
+                token: course_value
+            })
+    return stack[0] if stack else {}
+
+def parse_into_json():
+    with open('course_scraper/prereqs2.tsv') as myf:
+        courses = myf.readlines()
+    
+    # results = []
+    for course in courses:
+        # print(course)
+        s = course.rstrip('\n').split('\t')
+        # result = {
+        #     "course": s[0],
+        #     "grad_level": s[1],
+        #     "handbook_prereqs": s[2] if len(s) > 2 else '',
+        #     "prereqs": parse_tokens(reversed(s[3].split())) if len(s) > 3 else []
+        # }
+        # print("----")
+        # print(result)
+        # results.append(result)
+        cname = s[0]
+        aa[cname]['grad_level'] = s[1]
+        aa[cname]["handbook_prereqs"] = s[2] if len(s) > 2 else ''
+        aa[cname]['prereqs'] = parse_tokens(reversed(s[3].split())) if len(s) > 3 else []
+
+    print(json.dumps(aa))
+
+parse_into_json()
+
+
+
+    
--- server/course_scraper/put_in_db.py
+++ server/course_scraper/put_in_db.py
@@ -2,18 +2,20 @@
 import json
 import psycopg2
 
-with open("courses_nodupes.json") as myf:
+COURSE_FILE = "course_scraper/prereqs3.json"
+with open(COURSE_FILE) as myf:
     j = json.load(myf)
 
 conn = psycopg2.connect(dbname="postgres", user="postgres", host="127.0.0.1", port=5432, password="abc")
 cursor = conn.cursor()
-print(cursor.execute("SELECT COUNT(*) from courses;"))
-for course in j:
-    code = course['code']
-    name=course['name']
-    url =course['url']
-    outline = course['outline'].replace('\n', ' ') if 'outline' in course else ''
-    res = cursor.execute(f"""INSERT INTO COURSES VALUES (%s, %s, %s, %s);""", (code, name, url, outline))
+
+for course in j.keys():
+    print(course)
+    query = """UPDATE courses set (grad_level, handbook_prereqs, prereqs) = (%s, %s, %s) where course_code = %s""" 
+    grad_lvl = j[course]['grad_level']
+    handbook = j[course]['handbook_prereqs']
+    prereqs = json.dumps(j[course]['prereqs'])
+    cursor.execute(query, (grad_lvl, handbook, prereqs, course))
 
 conn.commit()
 
